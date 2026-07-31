f9e242e54ae3dd8078cb0ff8a31a05083fa76d11
benpastore <ben.pastore14@gmail.com>
2026-07-31 07:46:47 -0400

    update modified files

--- workflows/haplotype_aware/run_workflow.sh
+++ workflows/haplotype_aware/run_workflow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sh run.sh \
-    --design /fs/ess/PAS0631/BEN_PIPELINE/nextflow_lrs/workflows/haplotype_aware/debug_design_debug.txt \
-    --results /fs/scratch/PAS0631/AAA_DEBUG_HAPLO_AWARE/07_17_2026 -resume
+    --design /fs/ess/PAS0631/BEN_PIPELINE/nextflow_lrs/workflows/haplotype_aware/family_A_design.txt \
+    --results /fs/scratch/PAS0631/00_HAPLOTYPE_AWARE_ANALYSIS/FAMILY_A
