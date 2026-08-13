915ca5a0012b528cb9fb7526ca34a198415acf78
benpastore <ben.pastore14@gmail.com>
2026-08-13 12:16:29 -0400

    add resume to run_workflow.sh

--- workflows/haplotype_aware/run_workflow.sh
+++ workflows/haplotype_aware/run_workflow.sh
@@ -5,4 +5,5 @@ sh run.sh \
     --family_json /fs/ess/PAS0631/BEN_PIPELINE/nextflow_lrs/workflows/haplotype_aware/family.json \
     --run_trio_analysis true \
     --illumina_design /fs/ess/PAS0631/BEN_PIPELINE/nextflow_lrs/workflows/haplotype_aware/illumina_design.csv \
-    --results /fs/scratch/PAS0631/00_HAPLOTYPE_AWARE_ANALYSIS/FAMILY_A
+    --results /fs/scratch/PAS0631/00_HAPLOTYPE_AWARE_ANALYSIS/FAMILY_A \
+    -resume
