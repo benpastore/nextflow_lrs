32e2b78e69a27b932ab13904113c6cc8b2893fc7
benpastore <59121790+benpastore@users.noreply.github.com>
2026-08-17 11:26:36 -0400

    fix hero docker container

--- workflows/haplotype_aware/run_workflow.sh
+++ workflows/haplotype_aware/run_workflow.sh
@@ -4,6 +4,6 @@ sh run.sh \
     --design /fs/ess/PAS0631/BEN_PIPELINE/nextflow_lrs/workflows/haplotype_aware/family_A_design.txt \
     --family_json /fs/ess/PAS0631/BEN_PIPELINE/nextflow_lrs/workflows/haplotype_aware/family.json \
     --run_trio_analysis true \
-    --illumina_design /fs/ess/PAS0631/BEN_PIPELINE/nextflow_lrs/workflows/haplotype_aware/illumina_design.csv \
+    --illumina_design /fs/ess/PAS0631/BEN_PIPELINE/nextflow_lrs/workflows/haplotype_aware/illumina_design.csvt \
     --results /fs/scratch/PAS0631/00_HAPLOTYPE_AWARE_ANALYSIS/FAMILY_A \
     -resume
